951a0497f923689c8c189d2fe7cfe26f5de2ae51
Julio Cesar <julioreis044@gmail.com>
2022-11-15 15:29:07 -0300

    trabalhando com onmouseOver/onmouseout

--- main.js
+++ main.js
@@ -12,7 +12,39 @@ const mudarCor2 = () => {
 
 };
 
-//Praticando o mouseOver
+//Praticando o onmouseOver/onmouserout
+
+const cresceCaixa = () => {
+    
+    let width = document.querySelector('.caixa1').style.width = '200px';
+    let height= document.querySelector('.caixa1').style.height = '200px';
+
+}
+
+const desceCaixa = () => {
+
+    let width = document.querySelector('.caixa1').style.width = '150px';
+    let height= document.querySelector('.caixa1').style.height = '150px';
+
+}
+
+//caixa 2
+
+const cresceCaixa2 = () => {
+    
+    let width = document.querySelector('.caixa2').style.width = '200px';
+    let height= document.querySelector('.caixa2').style.height = '200px';
+
+}
+
+const desceCaixa2 = () => {
+
+    let width = document.querySelector('.caixa2').style.width = '150px';
+    let height= document.querySelector('.caixa2').style.height = '150px';
+
+}
+
+
 
 
 
