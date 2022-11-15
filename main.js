69ae2d3c6e542e956bc33d243773a970108acba8
Julio Cesar <julioreis044@gmail.com>
2022-11-15 12:33:35 -0300

    praticando eventos com JavaScript

---
+++ main.js
@@ -0,0 +1,22 @@
+//Praticando o Onclick
+
+const mudarCor1 = () => {
+    
+    let caixa1 = document.querySelector('.caixa1').style.backgroundColor = 'lightskyblue';
+    
+};
+
+const mudarCor2 = () => {
+    
+    let caixa1 = document.querySelector('.caixa2').style.backgroundColor = 'green';
+
+};
+
+//Praticando o mouseOver
+
+
+
+
+
+
+
